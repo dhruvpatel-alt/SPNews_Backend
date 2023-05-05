2c91c66c9a17c81397d623ff63dc83c318ae7ea5
DHRUV PATEL <69008045+dhruvpatel-alt@users.noreply.github.com>
2023-05-06 01:36:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(cors());
 app.use(express.json());
 //Availabe News
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, https://sp-news-frontend.vercel.app');
+ res.setHeader('Access-Control-Allow-Origin', 'https://sp-news-frontend.vercel.app');
+
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
