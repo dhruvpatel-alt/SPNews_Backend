e842d8584e3496bf0dea8bd092ce9ff371eaa57b
DHRUV PATEL <69008045+dhruvpatel-alt@users.noreply.github.com>
2023-09-18 23:55:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(cors());
 app.use(express.json());
 //Availabe News
 app.use((req, res, next) => {
- res.setHeader('Access-Control-Allow-Origin', '*');
 
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
